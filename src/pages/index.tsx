71acdd46c6d6b0c0403ab2e2f06bf0fba785e3bb
Honza Penicka <honza.penicka@gmail.com>
2024-04-27 11:59:42 +0200

    analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import HomePage from './home-page';
 
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
 		<main className='h-screen bg-white p-3 text-black '>
 			<HomePage />
+
+			<Analytics />
 		</main>
 	);
 }
