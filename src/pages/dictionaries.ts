31c546d8475c72c056adf257ae6acfd452cdc602
Jan Pěnička <jan.penicka@ceskatelevize.cz>
2024-09-14 22:45:51 +0200

    fix

--- src/pages/dictionaries.ts
+++ src/pages/dictionaries.ts
@@ -1,4 +1,4 @@
-import 'server-only';
+/*import 'server-only';
 
 const dictionaries = {
 	en: () => import('./locales/en.json').then((module) => module.default),
@@ -9,3 +9,4 @@ const dictionaries = {
 
 export const getDictionary = async (locale: string) =>
 	dictionaries[locale as 'en' | 'cs' | 'vi' | 'sk']();
+*/
